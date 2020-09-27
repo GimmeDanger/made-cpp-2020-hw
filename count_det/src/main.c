089b8fdd48e5dcb822b257e8dea5ebbe039447e0
GimmeDanger <vladim1r.nazarov@yandex.ru>
2020-09-27 22:46:22 +0300

    NEW: solved hw2: arm asm det

--- count_det/src/main.c
+++ count_det/src/main.c
@@ -1,22 +1,92 @@
-#include "stdio.h"
 #include "stdint.h"
 #include "stdlib.h"
+#include "stdio.h"
+#include "assert.h"
 
+/** Computes determinant of 3x3 matrix @numbers,
+*   ARM assembler version
+*/
+int32_t count_det(int32_t *numbers);
 
-int32_t count_det (int32_t* numbers);
+#define N 3
+#define ANS ((N) * (N))
+#define IDX(I, J) ((N) * (I) + (J))
 
-int main () {
-	int32_t* numbers = malloc(sizeof(int32_t) * 9);
-	
-	for (uint8_t i = 0; i < 9; ++ i)
-		scanf("%d", &numbers[i]);
-
-	const int32_t det = count_det(numbers);
-
-	printf("%d\n", det);
-
-	free(numbers);
-
-	return 0;
+/** Computes determinant of 3x3 matrix @numbers,
+*   C-version for unit tests
+*/
+int32_t count_det_test(int32_t *numbers) {
+  int32_t det = 0;
+  det += numbers[IDX(0, 0)] * numbers[IDX(1, 1)] * numbers[IDX(2, 2)];
+  det += numbers[IDX(0, 1)] * numbers[IDX(1, 2)] * numbers[IDX(2, 0)];
+  det += numbers[IDX(0, 2)] * numbers[IDX(1, 0)] * numbers[IDX(2, 1)];
+  det -= numbers[IDX(0, 0)] * numbers[IDX(1, 2)] * numbers[IDX(2, 1)];
+  det -= numbers[IDX(0, 1)] * numbers[IDX(1, 0)] * numbers[IDX(2, 2)];
+  det -= numbers[IDX(0, 2)] * numbers[IDX(1, 1)] * numbers[IDX(2, 0)];
+  return det;
 }
 
+void run_tests() {
+  {
+    int32_t numbers[N * N + 1] = {
+      1, 2, 3,
+      4, 5, 6,
+      7, 8, 9,
+      0
+    };
+    int32_t r0 = numbers[ANS];
+    int32_t r1 = count_det_test(numbers);
+    int32_t r2 = count_det(numbers);
+    if (r0 != r1 || r0 != r2) {
+      printf("Error: r0 = %d, r1 = %d, r2 = %d\n", r0, r1, r2);
+      assert(0);
+    }
+  }
+
+  {
+    int32_t numbers[N * N + 1] = {
+       1,  2,  -3,
+       4,  5,   6,
+      -7,  8,   9,
+      -360
+    };
+    int32_t r0 = numbers[ANS];
+    int32_t r1 = count_det_test(numbers);
+    int32_t r2 = count_det(numbers);
+    if (r0 != r1 || r0 != r2) {
+      printf("Error: r0 = %d, r1 = %d, r2 = %d\n", r0, r1, r2);
+      assert(0);
+    }
+  }
+
+  {
+    int32_t numbers[N * N + 1] = {
+      13,  2,  3,
+       4, 55,  6,
+      -2,  8,  1,
+      485
+    };
+    int32_t r0 = numbers[ANS];
+    int32_t r1 = count_det_test(numbers);
+    int32_t r2 = count_det(numbers);
+    if (r0 != r1 || r0 != r2) {
+      printf("Error: r0 = %d, r1 = %d, r2 = %d\n", r0, r1, r2);
+      assert(0);
+    }
+  }
+  printf("All tests passed!\n");
+}
+
+int main() {
+  run_tests();
+
+  int32_t *numbers = malloc(sizeof(int32_t) * 9);
+  for (uint8_t i = 0; i < 9; ++i)
+    scanf("%d", &numbers[i]);
+
+  const int32_t det = count_det(numbers);
+  printf("%d\n", det);
+  free(numbers);
+
+  return 0;
+}
--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,127 @@
-/// Your code goes here...
+.text
 
+.global count_det
+
+// Problem:
+// compute det of 3x3 matrix,
+// passed to function as int32_t* r0
+count_det:
+    // mat     idx
+    // a b c | 0 1 2
+    // d e f | 3 4 5
+    // g h i | 6 7 8
+    //
+    // det = a(ei − fh) − b(di − fg) + c(dh − eg)
+    
+    // usage of registers:
+    // r0 -- input array start
+    // r1 -- total sum of det
+    // r2 -- ptr for element
+    // r3 -- tmp
+
+    // det = 0
+    mov r1, #0
+
+    // det += aei
+    mov r2, #4
+    add r2, r0, r2, lsl #2
+    ldr r3, [r2]
+    //
+    mov r2, #8
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    //
+    mov r2, #0
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    add r1, r1, r3
+    
+    // det -= afh
+    mov r2, #5
+    add r2, r0, r2, lsl #2
+    ldr r3, [r2]
+    //
+    mov r2, #7
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    //
+    mov r2, #0
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    sub r1, r1, r3
+
+    // det -= bdi
+    mov r2, #3
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mov r3, r2
+    //
+    mov r2, #8
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    //
+    mov r2, #1
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    sub r1, r1, r3
+
+    // det += bfg
+    mov r2, #5
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mov r3, r2
+    //
+    mov r2, #6
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    //
+    mov r2, #1
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    add r1, r1, r3
+
+    // det += cdh
+    mov r2, #3
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mov r3, r2
+    //
+    mov r2, #7
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    //
+    mov r2, #2
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    add r1, r1, r3
+
+    // det -= ceg
+    mov r2, #4
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mov r3, r2
+    //
+    mov r2, #6
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    //
+    mov r2, #2
+    add r2, r0, r2, lsl #2
+    ldr r2, [r2]
+    mul r3, r3, r2
+    sub r1, r1, r3
+
+    // return
+    mov r0, r1
+    bx lr
